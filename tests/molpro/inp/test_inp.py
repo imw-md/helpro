b2fad8da6d5f7f19216a93434472add8d852f34d
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2025-01-15 12:24:27 +0100

    Make `data`

--- tests/molpro/inp/test_inp.py
+++ tests/molpro/inp/test_inp.py
@@ -2,17 +2,20 @@
 
 from pathlib import Path
 
+import pytest
+
 from helpro.molpro.inp import write
 
 
-def test_molpro(tmp_path: Path) -> None:
+@pytest.mark.parametrize(("method", "basis"), [("DF-HF", "cc-pVDZ")])
+def test_inp(method: str, basis: str, tmp_path: Path) -> None:
     """Test."""
-    p = Path(__file__).parent / "molpro.inp.ref"
+    p = Path(__file__).parent / "data" / method / basis / "molpro.inp"
     with p.open("r", encoding="utf-8") as f:
         sref = f.read()
 
     p = tmp_path / "molpro.inp"
-    write(method="DF-HF", basis="cc-pVDZ", fname=p)
+    write(method=method, basis=basis, fname=p)
     with p.open("r", encoding="utf-8") as f:
         s = f.read()
 
