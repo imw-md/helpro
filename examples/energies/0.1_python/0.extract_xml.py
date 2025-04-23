494b1b33f4dfde0ee6db918c49ecc97d115fef7f
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2025-04-23 08:32:50 +0200

    Update parsers

--- examples/energies/0.1_python/0.extract_xml.py
+++ examples/energies/0.1_python/0.extract_xml.py
@@ -5,38 +5,38 @@ import os
 from pathlib import Path
 
 import pandas as pd
-from helpro.molpro.xml import read_molpro_xml
-from helpro.molpro.bases import bases_all
 
+from helpro.molpro.bases import bases_all
+from helpro.molpro.xml import read_molpro_xml
 
 top = Path("../../molpro.2024.1_finished/0.1_python").resolve()
 
 
-def format_df(df) -> pd.DataFrame:
+def format_df(df: pd.DataFrame) -> pd.DataFrame:
+    """Format ``pd.DataFrame``."""
     for k in df.columns:
         if k == "name":
             df[k] = df[k].str.pad(28)
         elif df[k].dtype == "category":
             df[k] = df[k].str.pad(20)
-        elif df[k].dtype in [int]:
+        elif df[k].dtype == int:
             df[k] = df[k].astype(str).str.pad(4)
-        elif df[k].dtype in [bool]:
+        elif df[k].dtype == bool:
             df[k] = df[k].astype(str).str.pad(6)
         elif df[k].dtype == object:
             df[k] = df[k].str.pad(20)
     return df
 
 
-def collect():
+def collect() -> pd.DataFrame:
+    """Collect data."""
     ds = []
     for root, dirs, files in os.walk(top):
         if "molpro.xml" not in files:
             continue
-        filename = os.path.join(top, root, "molpro.xml")
+        filename = top / root / "molpro.out"
         print(filename)
         atoms = read_molpro_xml(filename)
-        # d["cpu_time"] = atoms.calc.results["cpu_time"]
-        # real_time = atoms.calc.results["real_time"]
         results = atoms.calc.results
         energy = results.get("energy", float("nan"))
         results.pop("free_energy", None)
@@ -53,8 +53,8 @@ def collect():
     return pd.DataFrame(ds)
 
 
-def main():
-    """Main"""
+def main() -> None:
+    """Command."""
     df = collect()
     df["basis"] = pd.Categorical(df["basis"], categories=bases_all, ordered=True)
     df = df.sort_values(["method", "basis", "core", "i"])
